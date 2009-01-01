31ff5fabc2f59548ecd8de506d675e8f74c66cb2
knipknap <knipknap@02ad1e36-d830-11dd-8ca8-e3ff79f713b6>
2009-01-01 18:33:40 +0000

    * Updated.

--- trunk/install.py
+++ trunk/install.py
@@ -48,8 +48,8 @@ gen_file(desktopfile)
 
 # Icon file names and installation locations.
 iconname                   = cfg["APP_SYSNAME"] + ".png"
-inst["pixmaps/icon32.png"] = cfg["INSTALL_ICON"] + "32x32/apps/" + iconname
-inst["pixmaps/icon48.png"] = cfg["INSTALL_ICON"] + "48x48/apps/" + iconname
+inst["pixmaps/icon32.png"] = os.path.join(cfg["INSTALL_ICON"], "32x32", "apps", iconname)
+inst["pixmaps/icon48.png"] = os.path.join(cfg["INSTALL_ICON"], "48x48", "apps", iconname)
 
 # Mime type definition file.
 mime_xml_file       = cfg["APP_SYSNAME"] + ".xml"
@@ -82,8 +82,8 @@ inst["src/*.py"] = cfg["INSTALL_LIB"]
 # Cleanup older installations.
 ##########################################
 print "Cleaning up older installations..."
-remove_dir(cfg["INSTALL_LIB_PFX"]   + "shriman/")
-remove_dir(cfg["INSTALL_SHARE_PFX"] + "shriman/")
+remove_dir(os.path.join(cfg["INSTALL_LIB_PFX"],   "shriman"))
+remove_dir(os.path.join(cfg["INSTALL_SHARE_PFX"], "shriman"))
 if os.path.isfile(cfg["INSTALL_BIN_FILE"]):
     os.remove(cfg["INSTALL_BIN_FILE"])
 
@@ -104,7 +104,7 @@ for pattern in inst:
 
 
 print "Changing permission of the root script to 755..."
-binfile = cfg["INSTALL_LIB"] + cfg["APP_SYSNAME"] + ".py"
+binfile = os.path.join(cfg["INSTALL_LIB"], cfg["APP_SYSNAME"] + ".py")
 os.chmod(binfile, stat.S_IRWXU
                 | stat.S_IRGRP | stat.S_IXGRP
                 | stat.S_IROTH | stat.S_IXOTH)
@@ -113,7 +113,7 @@ print "Creating application symlink..."
 os.symlink(binfile, cfg["INSTALL_BIN_FILE"])
 
 print "Running MIME database update..."
-os.system("update-mime-database " + cfg["INSTALL_MIME_PFX"])
+os.system("update-mime-database %s" % cfg["INSTALL_MIME_PFX"])
 
 print "Compiling the application..."
 os.chdir(cfg["INSTALL_LIB"])
--- trunk/src/config.py
+++ trunk/src/config.py
@@ -1,3 +1,4 @@
+from os.path import join
 c = {}
 
 # Application config.
@@ -8,22 +9,23 @@ c["GETTEXT_PACKAGE"] = c["APP_SYSNAME"]
 
 # File installation config - prefixes.
 c["INSTALL_PREFIX"]     = "/usr/local/"
-c["INSTALL_LIB_PFX"]    = c["INSTALL_PREFIX"] + "lib/"
-c["INSTALL_SHARE_PFX"]  = c["INSTALL_PREFIX"] + "share/"
-c["INSTALL_PIXMAP_PFX"] = c["INSTALL_PREFIX"] + "pixmaps/"
+c["INSTALL_LIB_PFX"]    = join(c["INSTALL_PREFIX"], "lib")
+c["INSTALL_SHARE_PFX"]  = join(c["INSTALL_PREFIX"], "share")
+c["INSTALL_PIXMAP_PFX"] = join(c["INSTALL_PREFIX"], "pixmaps")
 c["INSTALL_MIME_PFX"]   = "/usr/share/mime/"
 
 # Concrete installation directories.
-c["INSTALL_BIN"]        = c["INSTALL_PREFIX"]     + "bin/"
-c["INSTALL_LIB"]        = c["INSTALL_LIB_PFX"]    + c["APP_SYSNAME"] + "/"
-c["INSTALL_SHARE"]      = c["INSTALL_SHARE_PFX"]  + c["APP_SYSNAME"] + "/"
-c["INSTALL_PIXMAP"]     = c["INSTALL_PIXMAP_PFX"] + c["APP_SYSNAME"] + "/"
-c["INSTALL_ICON"]       = c["INSTALL_SHARE_PFX"]  + "icons/hicolor/"
-c["INSTALL_LOCALE"]     = c["INSTALL_SHARE_PFX"]  + "locale/"
-c["INSTALL_MIME_XML"]   = c["INSTALL_MIME_PFX"]   + "packages/"
+c["INSTALL_BIN"]        = join(c["INSTALL_PREFIX"],     "bin",            '')
+c["INSTALL_LIB"]        = join(c["INSTALL_LIB_PFX"],    c["APP_SYSNAME"], '')
+print c["INSTALL_BIN"]
+c["INSTALL_SHARE"]      = join(c["INSTALL_SHARE_PFX"],  c["APP_SYSNAME"], '')
+c["INSTALL_PIXMAP"]     = join(c["INSTALL_PIXMAP_PFX"], c["APP_SYSNAME"], '')
+c["INSTALL_ICON"]       = join(c["INSTALL_SHARE_PFX"],  "icons/hicolor",  '')
+c["INSTALL_LOCALE"]     = join(c["INSTALL_SHARE_PFX"],  "locale",         '')
+c["INSTALL_MIME_XML"]   = join(c["INSTALL_MIME_PFX"],   "packages",       '')
 c["INSTALL_MIME_KEYS"]  = "/usr/share/mime-info/"
 c["INSTALL_MENU_XDG"]   = "/usr/share/applications/"
 
 # Concrete files.
-c["INSTALL_BIN_FILE"]     = c["INSTALL_BIN"]   + c["APP_SYSNAME"]
-c["INSTALL_NUTRITION_DB"] = c["INSTALL_SHARE"] + "nutrition.db"
+c["INSTALL_BIN_FILE"]     = join(c["INSTALL_BIN"],   c["APP_SYSNAME"])
+c["INSTALL_NUTRITION_DB"] = join(c["INSTALL_SHARE"], "nutrition.db")
